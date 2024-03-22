d2397762d570cf3aa0bdb5f9542eada14b152a1f
Crowdbotics <team@crowdbotics.com>
2024-03-22 10:35:58 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "the-dating-app-47220",
+  url: "https://the-dating-app-47220.botics.co",
+  api: "https://the-dating-app-47220.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
